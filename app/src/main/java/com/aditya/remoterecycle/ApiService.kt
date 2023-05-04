84b1cd79f91cec1115dec6acc453fc5c3308b625
adity <adityaahmadnugraha091@gmail.com>
2023-05-04 11:01:33 +0700

    pembaharuan

--- app/src/main/java/com/aditya/remoterecycle/ApiService.kt
+++ app/src/main/java/com/aditya/remoterecycle/ApiService.kt
@@ -4,12 +4,10 @@ import retrofit2.Call
 import retrofit2.http.DELETE
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.QueryMap
-import java.lang.reflect.Parameter
 
 interface ApiService {
     @GET("photos")
@@ -23,35 +21,13 @@ interface ApiService {
     @FormUrlEncoded
     @POST("photos")
     fun createPhotos(
-//      @field:("albumId")albumId : String,
-//        @field:("title")title : String
     ): Call<List<Photo>>
 
 
     @FormUrlEncoded
     @PUT("photos")
     fun putPhotos(
-        //   @path("albumId")albumId: String,
-//        @field("thumbnailUrl")thumbnailUrl : String,
-//        @field("id")id : Int,
-//        @field("url")url : String
     ): Call<List<Photo>>
-
-
-//    @FormUrlEncoded
-//    @PATCH("photos")
-//    fun patchPhotos(
-//        @patchPhoto("albumId")albumId: String,
-//        @field("thumbnailUrl")thumbnailUrl : String,
-//    @field("id")id : Int,
-//    @field("url")url : String
-//    ): Call<PhotoRespon>
-//
-//    val albumId : String,
-//    val id : Int,
-//    val title: String,
-//    val url: String,
-//    val thumbnailUrl : String
 
 
     @FormUrlEncoded
